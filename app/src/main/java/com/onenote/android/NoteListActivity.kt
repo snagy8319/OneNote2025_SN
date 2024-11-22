539eeb10b5b113a2a7fec1df98e79e4d048d3a0a
Daniel Wohlmuth <daniel.wohlmuth@fitapp.info>
2024-11-22 14:17:29 +0100

    Added toolbar.

--- app/src/main/java/com/onenote/android/NoteListActivity.kt
+++ app/src/main/java/com/onenote/android/NoteListActivity.kt
@@ -7,5 +7,7 @@ class NoteListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_list)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
     }
 }
