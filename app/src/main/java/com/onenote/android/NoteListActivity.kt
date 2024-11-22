f9a5bbc8e9bf1279f0e684e189e1f67b3540b880
Daniel Wohlmuth <daniel.wohlmuth@fitapp.info>
2024-11-22 14:33:27 +0100

    Added menu_list.xml and ic_add.png.

--- app/src/main/java/com/onenote/android/NoteListActivity.kt
+++ app/src/main/java/com/onenote/android/NoteListActivity.kt
@@ -1,6 +1,9 @@
 package com.onenote.android
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class NoteListActivity : AppCompatActivity() {
@@ -9,5 +12,18 @@ class NoteListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_note_list)
 
         setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_list, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.add) {
+            Toast.makeText(this, "Add Note clicked", Toast.LENGTH_LONG).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
