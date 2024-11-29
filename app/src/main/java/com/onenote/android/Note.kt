56f81c32b5e9df202560387889df3e49efa9ba65
Daniel Wohlmuth <daniel.wohlmuth@fitapp.info>
2024-11-29 18:35:08 +0100

    Updated Note.kt and build.gradle.kts.

--- app/src/main/java/com/onenote/android/Note.kt
+++ app/src/main/java/com/onenote/android/Note.kt
@@ -7,5 +7,5 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "notes")
 data class Note(
     @ColumnInfo(name="title") var title: String,
-    @ColumnInfo(name="title") var message: String,
+    @ColumnInfo(name="message") var message: String,
     @PrimaryKey(autoGenerate = true) val id: Int = 0)
